2115f15c3c37d55021f37ddca21e4f2fb2122926
Fsher07 <furkansalimm@gmail.com>
2022-08-31 16:13:33 +0300

    Fetch rooms api

--- src/component/rooms/rooms.js
+++ src/component/rooms/rooms.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import Header from "../header/header";
+import { fetchRooms } from "../../redux/reducers/rooms/roomSlice";
+import './room.css';
 
 const Rooms = () => {
+    const dispatch = useDispatch();
+    const rooms = useSelector(state => state.rooms.rooms);
+    console.log('asd:', rooms);
+    useEffect(() => {
+        dispatch(fetchRooms());
+    } , [dispatch]);
     return (
         <>
             <Header />
             <div className="rooms">
+                {rooms.length > 0 ? rooms.map(room => (
+                    <div key={room.name} className="room">
+                        <div className="room-name">{room.name}</div>
+                        <div className="room-description">{room.description}</div>
+                    </div>
+                )) : <div>Loading...</div>}
                 <h1>Rooms</h1>
             </div>
         </>
