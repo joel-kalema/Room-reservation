a7206a2fb13d206f30d9a4a1dff19225169ff668
Fsher07 <furkansalimm@gmail.com>
2022-09-02 16:01:39 +0300

    Add room details extension

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import 'react-toastify/dist/ReactToastify.css';
+toast.configure()
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Reservations from './component/reservations/reservations';
 import Sessions from './component/sessions/sessions';
@@ -6,20 +8,26 @@ import Rooms from './component/rooms/rooms';
 import Header from './component/header/header';
 import Login from './component/auth/login/login';
 import Signup from './component/auth/sign up/signup';
+import Details from './component/rooms/roomDetail';
 
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Header />
-        <Routes>
-          <Route path='/login' element={<Login />} />
-          <Route path='/register' element={<Signup />} />
-          <Route path='/rooms' element={<Rooms />} />
-          <Route path='/sessions' element={<Sessions />} />
-          <Route path='/reservations' element={<Reservations />} />
-        </Routes>
+        <div>
+          <Header />
+          <div className='app_container'>
+            <Routes>
+              <Route path='/login' element={<Login />} />
+              <Route path='/register' element={<Signup />} />
+              <Route path='/' element={<Rooms />} />
+              <Route path='/sessions' element={<Sessions />} />
+              <Route path='/reservations' element={<Reservations />} />
+              <Route path='/room/:id' element={<Details />} />
+            </Routes>
+          </div>
+        </div>
       </Router>
     </div>
   );
